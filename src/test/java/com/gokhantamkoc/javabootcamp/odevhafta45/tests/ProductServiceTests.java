ab66778e091b500e445709ea1460a2946394a85d
alisimsek <ali.simsek035@gmail.com>
2022-07-17 15:41:29 +0300

    4.-5. hafta odevlerini tamamladım.

--- src/test/java/com/gokhantamkoc/javabootcamp/odevhafta45/tests/ProductServiceTests.java
+++ src/test/java/com/gokhantamkoc/javabootcamp/odevhafta45/tests/ProductServiceTests.java
@@ -14,6 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+
 public class ProductServiceTests {
 
     private ProductService productService;
